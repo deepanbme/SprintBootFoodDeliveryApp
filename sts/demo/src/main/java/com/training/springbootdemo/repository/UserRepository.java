a0f4a03aa56300721f0e1ae7e0a47b3fa40fbb7a
deepanbme <57568829+deepanbme@users.noreply.github.com>
2021-08-28 10:15:10 +0530

    Delete UserRepository.java

--- sts/demo/src/main/java/com/training/springbootdemo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.training.springbootdemo.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.training.springbootdemo.model.User;
-
-public interface UserRepository extends CrudRepository<User, Integer>{
-
-}
